02786e38a234453ca40d170f13488b2e92aee61f
leng003 <71398390+leng003@users.noreply.github.com>
2020-09-26 15:11:06 -0400

    added educational links

---
+++ frontend/src/EducationalLinks.jsx
@@ -0,0 +1,15 @@
+import React, { PureComponent, Fragment } from 'react';
+import { Container, Grid, Image } from 'semantic-ui-react';
+
+export default class EducationalLinks extends PureComponent {
+	render() {
+		return (
+			<Fragment>
+				<h1>Educational Links</h1>
+				<Container>
+					<p></p>
+				</Container>
+			</Fragment>
+		);
+	}
+}
