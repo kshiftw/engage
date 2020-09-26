c8ceb775214de118e833fd68cc18a4f04ffb1423
mkadiya20 <mkadiya20@yahoo.com>
2020-09-26 02:29:09 -0400

    Update database.js

--- src/database.js
+++ src/database.js
@@ -32,11 +32,26 @@ const userSchema = new Schema(
 const subjectSchema = new Schema(
 	{
 		// add fields here
+		subjectName: String,
+		topics: [{
+				topicName: String,
+				urls: [String]
+				}]
 	},
 	{
 		timestamps: true,
 	}
 );
+
+// Create a new subject in the database
+function createNewSubject(){
+	Subject.insertMany([
+		{
+		 subjectName: "Math",
+		 topics: [{topicName: "Calculus", urls: "https://www.google.com/"}]
+		}
+	], function(err, urlName){});
+}
 
 const User = mongoose.model('User', userSchema);
 const Subject = mongoose.model('Subject', subjectSchema);
