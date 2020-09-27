f111d29f2c2e1ad3c3812c866e2988e43c6b98be
kwong <kirkwong33@gmail.com>
2020-09-27 06:00:23 -0700

    Merged changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,184 +1,184 @@
-import React, { PureComponent } from "react";
-import "./App.css";
+import React, { PureComponent } from 'react';
+import './App.css';
 
-import profile from "./images/avatar.png";
+import profile from './images/avatar.png';
 
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { Container } from "semantic-ui-react";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
 
-import Navbar from "./Navbar";
-import Landing from "./Landing";
-import Login from "./Login";
-import Profile from "./Profile";
-import EducationalLinks from "./EducationalLinks";
-import Register from "./Register";
-import About from "./About";
-import Grow from "./Grow";
+import Navbar from './Navbar';
+import Landing from './Landing';
+import Login from './Login';
+import Profile from './Profile';
+import EducationalLinks from './EducationalLinks';
+import Register from './Register';
+import About from './About';
+import Grow from './Grow';
 
 export default class App extends PureComponent {
-  constructor(props) {
-    super(props);
-  }
-  about = [
-    {
-      name: "Mihir",
-      profilePicture: profile,
-      info: "Student of Computer Science at Western University",
-    },
-    {
-      name: "Lenny",
-      profilePicture: profile,
-      info: "Electrical Engineering at FIU",
-    },
-    {
-      name: "Kirk",
-      profilePicture: profile,
-      info: "Lorem............",
-    },
-    {
-      name: "Israel",
-      profilePicture: profile,
-      info: "Student of Computer Science at Hunter College",
-    },
-  ];
-  state = {
-    // REACT_APP_API_PATH = localhost:3030 // <- use this when running in local environment to avoid building frontend
-    apiPath: process.env.REACT_APP_API_PATH + "/api",
+	constructor(props) {
+		super(props);
+	}
+	about = [
+		{
+			name: 'Mihir',
+			profilePicture: profile,
+			info: 'Student of Computer Science at Western University',
+		},
+		{
+			name: 'Lenny',
+			profilePicture: profile,
+			info: 'Electrical Engineering at FIU',
+		},
+		{
+			name: 'Kirk',
+			profilePicture: profile,
+			info: 'Computer Systems Technology student at BCIT',
+		},
+		{
+			name: 'Israel',
+			profilePicture: profile,
+			info: 'Student of Computer Science at Hunter College',
+		},
+	];
+	state = {
+		// REACT_APP_API_PATH = localhost:3030 // <- use this when running in local environment to avoid building frontend
+		apiPath: process.env.REACT_APP_API_PATH + '/api',
 
-    // Use this when having backend server frontend (when deploying)
-    // apiPath: '/api',
+		// Use this when having backend server frontend (when deploying)
+		// apiPath: '/api',
 
-    selfUser: null,
-    loadingSelf: true,
-    inLobby: false,
-    about: this.about,
-  };
+		selfUser: null,
+		loadingSelf: true,
+		inLobby: false,
+		about: this.about,
+	};
 
-  async componentDidMount() {
-    // Try to retrieve the login token
-    const token = localStorage.getItem("token");
-    if (!token) {
-      return this.setState({
-        loadingSelf: false,
-      });
-    }
+	async componentDidMount() {
+		// Try to retrieve the login token
+		const token = localStorage.getItem('token');
+		if (!token) {
+			return this.setState({
+				loadingSelf: false,
+			});
+		}
 
-    const { apiPath } = this.state;
+		const { apiPath } = this.state;
 
-    try {
-      // Authenticate the token and user using API route
-      const res = await fetch(`${apiPath}/users/self`, {
-        headers: {
-          Authorization: token,
-        },
-      });
-      if (!res.ok) throw res.status;
-      const { user } = await res.json();
+		try {
+			// Authenticate the token and user using API route
+			const res = await fetch(`${apiPath}/users/self`, {
+				headers: {
+					Authorization: token,
+				},
+			});
+			if (!res.ok) throw res.status;
+			const { user } = await res.json();
 
-      this.setState({
-        selfUser: user,
-        loadingSelf: false,
-      });
-    } catch (err) {
-      console.log(err);
-      this.setState({
-        loadingSelf: false,
-      });
-    }
-  }
+			this.setState({
+				selfUser: user,
+				loadingSelf: false,
+			});
+		} catch (err) {
+			console.log(err);
+			this.setState({
+				loadingSelf: false,
+			});
+		}
+	}
 
-  setSelfUser(selfUser, token) {
-    this.setState({
-      selfUser,
-    });
-  }
+	setSelfUser(selfUser, token) {
+		this.setState({
+			selfUser,
+		});
+	}
 
-  render() {
-    const { apiPath, selfUser, loadingSelf, about } = this.state;
+	render() {
+		const { apiPath, selfUser, loadingSelf, about } = this.state;
 
-    // If still loading, render a loader on the page
-    if (loadingSelf) {
-      return <div className="ui massive active loader" />;
-    }
+		// If still loading, render a loader on the page
+		if (loadingSelf) {
+			return <div className='ui massive active loader' />;
+		}
 
-    const setSelfUser = this.setSelfUser.bind(this);
+		const setSelfUser = this.setSelfUser.bind(this);
 
-    return (
-      <Container className="app_container">
-        <BrowserRouter>
-          <Navbar selfUser={selfUser} setSelfUser={setSelfUser} />
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={(props) => (
-                <Landing {...props} apiPath={apiPath} selfUser={selfUser} />
-              )}
-            />
-            <Route
-              exact
-              path="/login"
-              render={(props) => (
-                <Login
-                  {...props}
-                  apiPath={apiPath}
-                  selfUser={selfUser}
-                  setSelfUser={setSelfUser}
-                />
-              )}
-            />
-            <Route
-              exact
-              path="/about"
-              render={(props) => (
-                <About
-                  {...props}
-                  apiPath={apiPath}
-                  selfUser={selfUser}
-                  about={about}
-                />
-              )}
-            />
-            <Route
-              exact
-              path="/profile"
-              render={(props) => (
-                <Profile {...props} apiPath={apiPath} selfUser={selfUser} />
-              )}
-            />
-            <Route
-              exact
-              path="/grow"
-              render={(props) => (
-                <Grow {...props} apiPath={apiPath} selfUser={selfUser} />
-              )}
-            />
-            <Route
-              exact
-              path="/EducationalLinks"
-              render={(props) => (
-                <EducationalLinks
-                  {...props}
-                  apiPath={apiPath}
-                  selfUser={selfUser}
-                />
-              )}
-            />
-            <Route
-              exact
-              path="/register"
-              render={(props) => (
-                <Register
-                  {...props}
-                  apiPath={apiPath}
-                  selfUser={selfUser}
-                  setSelfUser={setSelfUser}
-                />
-              )}
-            />
-          </Switch>
-        </BrowserRouter>
-      </Container>
-    );
-  }
+		return (
+			<Container className='app_container'>
+				<BrowserRouter>
+					<Navbar selfUser={selfUser} setSelfUser={setSelfUser} />
+					<Switch>
+						<Route
+							exact
+							path='/'
+							render={(props) => (
+								<Landing {...props} apiPath={apiPath} selfUser={selfUser} />
+							)}
+						/>
+						<Route
+							exact
+							path='/login'
+							render={(props) => (
+								<Login
+									{...props}
+									apiPath={apiPath}
+									selfUser={selfUser}
+									setSelfUser={setSelfUser}
+								/>
+							)}
+						/>
+						<Route
+							exact
+							path='/about'
+							render={(props) => (
+								<About
+									{...props}
+									apiPath={apiPath}
+									selfUser={selfUser}
+									about={about}
+								/>
+							)}
+						/>
+						<Route
+							exact
+							path='/profile'
+							render={(props) => (
+								<Profile {...props} apiPath={apiPath} selfUser={selfUser} />
+							)}
+						/>
+						<Route
+							exact
+							path='/grow'
+							render={(props) => (
+								<Grow {...props} apiPath={apiPath} selfUser={selfUser} />
+							)}
+						/>
+						<Route
+							exact
+							path='/EducationalLinks'
+							render={(props) => (
+								<EducationalLinks
+									{...props}
+									apiPath={apiPath}
+									selfUser={selfUser}
+								/>
+							)}
+						/>
+						<Route
+							exact
+							path='/register'
+							render={(props) => (
+								<Register
+									{...props}
+									apiPath={apiPath}
+									selfUser={selfUser}
+									setSelfUser={setSelfUser}
+								/>
+							)}
+						/>
+					</Switch>
+				</BrowserRouter>
+			</Container>
+		);
+	}
 }
--- frontend/src/Grow.jsx
+++ frontend/src/Grow.jsx
@@ -1,225 +1,225 @@
-import React, { PureComponent, Fragment } from "react";
-import { Redirect } from "react-router-dom";
-import { Container, Button, Input, Card, Image } from "semantic-ui-react";
-import { CircularProgressbarWithChildren } from "react-circular-progressbar";
-import NumericInput from "react-numeric-input";
-import "react-circular-progressbar/dist/styles.css";
+import React, { PureComponent, Fragment } from 'react';
+import { Redirect } from 'react-router-dom';
+import { Container, Button, Input, Card, Image } from 'semantic-ui-react';
+import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
+import NumericInput from 'react-numeric-input';
+import 'react-circular-progressbar/dist/styles.css';
 
-import Tree0 from "./images/tree_0.JPG";
-import Tree1 from "./images/tree_1.JPG";
-import Tree2 from "./images/tree_2.JPG";
-import Tree3 from "./images/tree_3.JPG";
-import Tree4 from "./images/tree_4.JPG";
-import Tree5 from "./images/tree_5.JPG";
+import Tree0 from './images/tree_0.JPG';
+import Tree1 from './images/tree_1.JPG';
+import Tree2 from './images/tree_2.JPG';
+import Tree3 from './images/tree_3.JPG';
+import Tree4 from './images/tree_4.JPG';
+import Tree5 from './images/tree_5.JPG';
 
 export default class Grow extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.tick = this.tick.bind(this);
-    this.startCountDown = this.startCountDown.bind(this);
-    this.resetTimer = this.resetTimer.bind(this);
-  }
-
-  state = {
-    timer_set: 0,
-    timer_remaining: 0,
-    timer_percentage: 0,
-    hour_remaining: 0,
-    minute_remaining: 0,
-    second_remaining: 0,
-    hour: 0,
-    minute: 0,
-    timer_started: false,
-  };
-
-  onHourChange(event) {
-    this.setState({ hour: event });
-  }
-
-  onMinuteChange(event) {
-    this.setState({ minute: event });
-  }
-
-  async updateTimer(time_elapsed, username) {
-	const { apiPath } = this.props;
-
-	const res = await fetch(`${apiPath}/update_timer`, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify({
-			username,
-			time_elapsed,
-		}),
-	});
-	if (!res.ok) throw await res.json();
-
-	return await res.json();
-}
-
-async tick() {
-	const { selfUser } = this.props;
-	let username = selfUser.username;
-    this.setState((prevState) => {
-      return { timer_remaining: prevState.timer_remaining - 1000 };
-    });
-
-    const { timer_set, timer_remaining } = this.state;
-
-    // https://stackoverflow.com/questions/19700283/how-to-convert-time-milliseconds-to-hours-min-sec-format-in-javascript
-    let minutes = Math.floor((timer_remaining / (1000 * 60)) % 60);
-    let hours = Math.floor((timer_remaining / (1000 * 60 * 60)) % 24);
-    let seconds = Math.floor((timer_remaining / 1000) % 60);
-    hours = hours < 10 ? "0" + hours : hours;
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    let new_timer_percentage = parseInt(
-      ((timer_set - timer_remaining) / timer_set) * 100
-    );
-
-    if (new_timer_percentage < 2) {
-      new_timer_percentage = 1;
-    }
-
-    this.setState({
-      timer_percentage: new_timer_percentage,
-      hour_remaining: hours,
-      minute_remaining: minutes,
-      second_remaining: seconds,
-	});
-	
-	if (timer_remaining <= 0) {
-		clearInterval(this.intervalHandle);
-		// store timer_set into db
-		// send username and time elapsed
-
-		try {
-			await this.updateTimer(timer_set, username);
-		} catch (err) {
-			console.log(err);
-		}
+	constructor(props) {
+		super(props);
+		this.tick = this.tick.bind(this);
+		this.startCountDown = this.startCountDown.bind(this);
+		this.resetTimer = this.resetTimer.bind(this);
 	}
-  }
 
-  startCountDown() {
-    const { hour, minute } = this.state;
-    let new_timer = hour * 3600000 + minute * 60000;
-    this.setState({
-      timer_set: new_timer,
-      timer_remaining: new_timer,
-      timer_started: true,
-    });
-
-    this.intervalHandle = setInterval(this.tick, 1000);
-  }
-
-async resetTimer() {
-	clearInterval(this.intervalHandle);
-	const { selfUser } = this.props;
-	let username = selfUser.username;
-
-	const { timer_set, timer_remaining } = this.state;
-	this.setState({
+	state = {
+		timer_set: 0,
 		timer_remaining: 0,
 		timer_percentage: 0,
 		hour_remaining: 0,
 		minute_remaining: 0,
 		second_remaining: 0,
+		hour: 0,
+		minute: 0,
 		timer_started: false,
-	});
+	};
 
-	try {
-		await this.updateTimer(timer_set - timer_remaining, username);
-	} catch (err) {
-		console.log(err);
+	onHourChange(event) {
+		this.setState({ hour: event });
 	}
-} 
 
-  render() {
-    const { selfUser } = this.props;
-    const {
-      timer_percentage,
-      hour_remaining,
-      minute_remaining,
-      second_remaining,
-      timer_started,
-    } = this.state;
-    // if (!selfUser) {
-    // 	return <Redirect to='/' />;
-    // }
+	onMinuteChange(event) {
+		this.setState({ minute: event });
+	}
 
-    let tree;
-    if (timer_percentage >= 0 && timer_percentage < 20) {
-      tree = <Image size="medium" circular src={Tree0}></Image>;
-    } else if (timer_percentage >= 20 && timer_percentage < 40) {
-      tree = <Image size="medium" circular src={Tree1}></Image>;
-    } else if (timer_percentage >= 40 && timer_percentage < 60) {
-      tree = <Image size="medium" circular src={Tree2}></Image>;
-    } else if (timer_percentage >= 60 && timer_percentage < 80) {
-      tree = <Image size="medium" circular src={Tree3}></Image>;
-    } else if (timer_percentage >= 80 && timer_percentage < 99) {
-      tree = <Image size="medium" circular src={Tree4}></Image>;
-    } else if (timer_percentage >= 99) {
-      tree = <Image size="medium" circular src={Tree5}></Image>;
-    }
+	async updateTimer(time_elapsed, username) {
+		const { apiPath } = this.props;
 
-    return (
-      <Fragment>
-        <div className="timer-container">
-          <h1>Grow</h1>
+		const res = await fetch(`${apiPath}/update_timer`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				username,
+				time_elapsed,
+			}),
+		});
+		if (!res.ok) throw await res.json();
 
-          {timer_started ? (
-            <p>
-              {hour_remaining}:{minute_remaining}:{second_remaining}{" "}
-            </p>
-          ) : (
-            <p className="time">00:00:00</p>
-          )}
-          <p className="hours-minutes">
-            Hours &nbsp;
-            <NumericInput
-              min={0}
-              max={24}
-              placeholder={0}
-              size={3}
-              onChange={this.onHourChange.bind(this)}
-            />
-            &nbsp;&nbsp;&nbsp;&nbsp; Minutes &nbsp;
-            <NumericInput
-              min={0}
-              max={60}
-              placeholder={0}
-              size={3}
-              onChange={this.onMinuteChange.bind(this)}
-            />
-          </p>
-          <p></p>
-          <div className="start-reset">
-            {!timer_started ? (
-              <Button onClick={this.startCountDown}>Start</Button>
-            ) : (
-              <Button disabled>Start</Button>
-            )}
+		return await res.json();
+	}
 
-            {timer_started ? (
-              <Button onClick={this.resetTimer}>Reset</Button>
-            ) : (
-              <Button disabled>Reset</Button>
-            )}
-          </div>
-        </div>
+	async tick() {
+		const { selfUser } = this.props;
+		let username = selfUser.username;
+		this.setState((prevState) => {
+			return { timer_remaining: prevState.timer_remaining - 1000 };
+		});
 
-        <div className="progressbar-container">
-          <CircularProgressbarWithChildren
-            value={timer_percentage}
-            strokeWidth={5}
-          >
-            {tree}
-          </CircularProgressbarWithChildren>
-        </div>
-      </Fragment>
-    );
-  }
+		const { timer_set, timer_remaining } = this.state;
+
+		// https://stackoverflow.com/questions/19700283/how-to-convert-time-milliseconds-to-hours-min-sec-format-in-javascript
+		let minutes = Math.floor((timer_remaining / (1000 * 60)) % 60);
+		let hours = Math.floor((timer_remaining / (1000 * 60 * 60)) % 24);
+		let seconds = Math.floor((timer_remaining / 1000) % 60);
+		hours = hours < 10 ? '0' + hours : hours;
+		minutes = minutes < 10 ? '0' + minutes : minutes;
+		seconds = seconds < 10 ? '0' + seconds : seconds;
+
+		let new_timer_percentage = parseInt(
+			((timer_set - timer_remaining) / timer_set) * 100
+		);
+
+		if (new_timer_percentage < 2) {
+			new_timer_percentage = 1;
+		}
+
+		this.setState({
+			timer_percentage: new_timer_percentage,
+			hour_remaining: hours,
+			minute_remaining: minutes,
+			second_remaining: seconds,
+		});
+
+		if (timer_remaining <= 0) {
+			clearInterval(this.intervalHandle);
+			// store timer_set into db
+			// send username and time elapsed
+
+			try {
+				await this.updateTimer(timer_set, username);
+			} catch (err) {
+				console.log(err);
+			}
+		}
+	}
+
+	startCountDown() {
+		const { hour, minute } = this.state;
+		let new_timer = hour * 3600000 + minute * 60000;
+		this.setState({
+			timer_set: new_timer,
+			timer_remaining: new_timer,
+			timer_started: true,
+		});
+
+		this.intervalHandle = setInterval(this.tick, 1000);
+	}
+
+	async resetTimer() {
+		clearInterval(this.intervalHandle);
+		const { selfUser } = this.props;
+		let username = selfUser.username;
+
+		const { timer_set, timer_remaining } = this.state;
+		this.setState({
+			timer_remaining: 0,
+			timer_percentage: 0,
+			hour_remaining: 0,
+			minute_remaining: 0,
+			second_remaining: 0,
+			timer_started: false,
+		});
+
+		try {
+			await this.updateTimer(timer_set - timer_remaining, username);
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
+	render() {
+		const { selfUser } = this.props;
+		const {
+			timer_percentage,
+			hour_remaining,
+			minute_remaining,
+			second_remaining,
+			timer_started,
+		} = this.state;
+		// if (!selfUser) {
+		// 	return <Redirect to='/' />;
+		// }
+
+		let tree;
+		if (timer_percentage >= 0 && timer_percentage < 20) {
+			tree = <Image size='medium' circular src={Tree0}></Image>;
+		} else if (timer_percentage >= 20 && timer_percentage < 40) {
+			tree = <Image size='medium' circular src={Tree1}></Image>;
+		} else if (timer_percentage >= 40 && timer_percentage < 60) {
+			tree = <Image size='medium' circular src={Tree2}></Image>;
+		} else if (timer_percentage >= 60 && timer_percentage < 80) {
+			tree = <Image size='medium' circular src={Tree3}></Image>;
+		} else if (timer_percentage >= 80 && timer_percentage < 99) {
+			tree = <Image size='medium' circular src={Tree4}></Image>;
+		} else if (timer_percentage >= 99) {
+			tree = <Image size='medium' circular src={Tree5}></Image>;
+		}
+
+		return (
+			<Fragment>
+				<div className='timer-container'>
+					<h1>Grow</h1>
+
+					{timer_started ? (
+						<p className='time'>
+							{hour_remaining}:{minute_remaining}:{second_remaining}{' '}
+						</p>
+					) : (
+						<p className='time'>00:00:00</p>
+					)}
+					<p className='hours-minutes'>
+						Hours &nbsp;
+						<NumericInput
+							min={0}
+							max={24}
+							placeholder={0}
+							size={3}
+							onChange={this.onHourChange.bind(this)}
+						/>
+						&nbsp;&nbsp;&nbsp;&nbsp; Minutes &nbsp;
+						<NumericInput
+							min={0}
+							max={60}
+							placeholder={0}
+							size={3}
+							onChange={this.onMinuteChange.bind(this)}
+						/>
+					</p>
+					<p></p>
+					<div className='start-reset'>
+						{!timer_started ? (
+							<Button onClick={this.startCountDown}>Start</Button>
+						) : (
+							<Button disabled>Start</Button>
+						)}
+
+						{timer_started ? (
+							<Button onClick={this.resetTimer}>Reset</Button>
+						) : (
+							<Button disabled>Reset</Button>
+						)}
+					</div>
+				</div>
+
+				<div className='progressbar-container'>
+					<CircularProgressbarWithChildren
+						value={timer_percentage}
+						strokeWidth={5}
+					>
+						{tree}
+					</CircularProgressbarWithChildren>
+				</div>
+			</Fragment>
+		);
+	}
 }
