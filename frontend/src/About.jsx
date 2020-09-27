3f8087e2828d454d3cf70c185f59dbb5696e985e
israx <70438514+israx@users.noreply.github.com>
2020-09-27 06:40:28 -0400

    new changes

--- frontend/src/About.jsx
+++ frontend/src/About.jsx
@@ -1,15 +1,29 @@
-import React, { PureComponent, Fragment } from 'react';
-import { Container, Grid, Image } from 'semantic-ui-react';
+import React, { PureComponent, Fragment } from "react";
+import { Container, Grid, Image } from "semantic-ui-react";
+import "./about.css";
 
 export default class About extends PureComponent {
-	render() {
-		return (
-			<Fragment>
-				<h1>About</h1>
-				<Container>
-					<p></p>
-				</Container>
-			</Fragment>
-		);
-	}
+  render() {
+    const about = this.props.about.map((person) => (
+      <div Class="user-card">
+        <img
+          src={person.profilePicture}
+          alt="profile-picture"
+          style={{ width: 150, borderRadius: "1000px" }}
+        />
+        <h1>{person.name}</h1>
+        <p>{person.info}</p>
+      </div>
+    ));
+    console.log(about);
+    return (
+      <Fragment>
+        <h1 class="meet-team">Meet the team</h1>
+        <p>Some description.....</p>
+        <Container>
+          <div className="users-container">{about}</div>
+        </Container>
+      </Fragment>
+    );
+  }
 }
